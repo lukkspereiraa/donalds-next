843dccbb27b6fee4a5507e6d332306f46dd2bd21
lukkspereiraa <lukkspereiraa@gmail.com>
2025-02-23 11:59:09 -0300

    feat: ensure same producr can be addead more than onece to cart

--- src/app/[slug]/menu/contexts/cart.tsx
+++ src/app/[slug]/menu/contexts/cart.tsx
@@ -30,7 +30,23 @@ export const CartProvider = ({ children }: { children: React.ReactNode }) => {
         setIsOpen((prev) => !prev)
     }
     const addProduct = (product: CartProduct) => {
-        setProducts((prev) => [...prev, product])
+
+        const productIsAlreadyInCart = products.some((prevProduct) => prevProduct.id === product.id)
+        if (!productIsAlreadyInCart) {
+            return setProducts((prev) => [...prev, product])
+        }
+
+        setProducts((prevProduct) => {
+            return prevProduct.map(prevProduct => {
+                if (prevProduct.id === product.id) {
+                    return {
+                        ...prevProduct,
+                        quantity: prevProduct.quantity + product.quantity
+                    }
+                }
+                return prevProduct
+            })
+        })
     }
     return (
         <CartContext.Provider
